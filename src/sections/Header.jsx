49278be5ab904153018f6d9a4df003de0b3dd84c
The Angry Coder <79235452+TheAngryCoder2@users.noreply.github.com>
2024-12-27 16:37:52 +0000

    wip

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link as LinkScroll } from 'react-scroll'
+import clsx from 'clsx'
+
 
 const NavLink = ({ title }) => (
     <LinkScroll
@@ -20,8 +22,12 @@ const Header = () => {
             <a className='lg:hidden flex-1 cursor-pointer z-2'>
                 <img src="/images/xora.svg" alt="logo" width={115} height={55} />
             </a>
-            <div className='w-full max-lg:fixed max-lg:top-0 max-lg:left-0
-            max-lg:w-full max-lg:bg-s2 max-lg:opacity-0'>
+
+            <div className={clsx(
+                'w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0', 
+                isOpen ? 
+                'max-lg:opacity-100' :
+                'max-lg:pointer-events-none')} >
 
             
             <div className='w-full max-lg:relative max-lg:flex-col 
